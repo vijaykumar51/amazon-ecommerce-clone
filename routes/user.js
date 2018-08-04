160c9e4110ba4e86e3bc4af245e5a42247ebc6db
Vijay Kumar <vijay.kumar.em51@gmail.com>
2018-08-04 10:38:44 +0530

    separating routes, integrating ejs

---
+++ routes/user.js
@@ -0,0 +1,33 @@
+var router = require("express").Router();
+var User = require("../models/user");
+
+router.post("/signup", (req, res, next) => {
+	var user = new User();
+
+	user.email = req.body.email;
+	user.password = req.body.password;
+	user.profile.name = req.body.name;
+
+	User.findOne({ email: user.email }, function(err, userExists) {
+		if (err) {
+			console.log(err);
+			return next(err);
+		}
+
+		if (userExists) {
+			console.log(`${user.email} alreay exists`);
+			res.redirect("/signup");
+		} else {
+			user.save(err => {
+				if (err) {
+					console.log(err);
+					return next(err);
+				} else {
+					res.status(200).json("User successfully created");
+				}
+			});
+		}
+	});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-
-var User = require("./models/user");
+const ejs = require("ejs");
+const ejsMate = require("ejs-mate");
 
 const app = express();
 
@@ -21,31 +21,19 @@ mongoose.connect(
 	}
 );
 
+app.use(express.static(__dirname + "/public"));
 app.use(morgan("dev"));
 // TODO: read about body-parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-	res.send("Hello user");
-});
+var mainRoutes = require("./routes/main.js");
+var userRoutes = require("./routes/user.js");
 
-app.post("/create-user", (req, res, next) => {
-	var user = new User();
-
-	user.email = req.body.email;
-	user.password = req.body.password;
-	user.profile.name = req.body.name;
-
-	user.save(err => {
-		if (err) {
-			console.log(err);
-			res.status(502).json("Error creating user");
-		} else {
-			res.status(200).json("User successfully created");
-		}
-	});
-});
+app.use(mainRoutes);
+app.use(userRoutes);
 
 app.listen(4000, err => {
 	if (err) {
